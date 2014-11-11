816a90174875c353620708064942317f95429ef5
nathairtras <nathairtras@live.com>
2014-11-11 10:10:50 -0800

    Initial installation script

---
+++ install.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FLDR="$(basename $DIR)"
+
+#tmux
+ln -s -f $DIR/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s -f $DIR/tmux $HOME/.tmux
+
+#Add the environment settings
+echo "Please add the following to your profile script:"
+echo ""
+echo "for i in \${HOME}/${FLDR}/env/*; do"
+echo '    if [ -r "$i" ]; then '
+echo '        source $i        '
+echo '    fi                   '
+echo 'done                     '
