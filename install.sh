4790c7869cc571bfb48454916169df4bbffc24b1
nathairtras <nathairtras@live.com>
2014-11-19 16:24:28 -0800

    add nanorc

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FLDR="$(basename $DIR)"
 
+#nano
+ln -s -f #DIR/dotfiles/tmux.conf $HOME/.tmux.conf
+
 #tmux
 ln -s -f $DIR/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s -f $DIR/tmux $HOME/.tmux
